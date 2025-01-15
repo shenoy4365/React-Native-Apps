0742be64c4619ab8115287cc868eb7f4e549211d
byteNinja112 <arnav.shenoy@gmail.com>
2025-01-15 11:05:21 -0600

    learned about TouchableOpacity and TouchableHighlight

--- secondApp/app/index.tsx
+++ secondApp/app/index.tsx
@@ -1,14 +1,33 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Button, TouchableHighlight, Alert, TouchableOpacity } from "react-native";
 
 
 export default function Index() {
+
+    const myAlertFn = () => {
+      Alert.alert("title", "message", [
+        { text: "Yes", onPress: () => console.log("Pressed yes") },
+        { text: "No", onPress: () => console.log("Pressed no") },
+      ]);
+    };
+
+
   return (
     <View style={styles.container}>
-      {/* <Text style={styles.headerText}>Header</Text>
-      <Text style={styles.text}>Text</Text> */}
-      <View style={styles.innerBox}><Text>A</Text></View>
-      <View style={styles.innerBox}><Text>B</Text></View>
-      <View style={styles.innerBox}><Text>C</Text></View>
+      <Text style={styles.text}>Hi 4th Hour</Text>
+      <Button title='click me' onPress={() => alert('hi')}/>
+      <TouchableHighlight 
+        style = {styles.highlightButton} 
+        onPress = {() => console.log("Pressed TH")} 
+        underlayColor = 'hotpink'
+      >
+        <Text style={styles.buttontext}> Touchable Highlight</Text>
+      </TouchableHighlight>
+
+      <TouchableOpacity
+        style = {styles.highlightButton} 
+        onPress = {myAlertFn}>
+        <Text>Click to see an alert</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -16,40 +35,23 @@ export default function Index() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, // fills the space
-    flexDirection: 'row', // arranging the children in the container 
-    padding: 15,
-    margin: 20,
+    flex: 1, 
     backgroundColor: 'lightblue',
-    //alignItems: 'center', // think like global scope impact,
-    justifyContent: 'center',
+    padding: 10,
   },
   text: {
-    fontSize: 20,
+    fontSize: 10,
   },
-  headerText: {
-    fontSize: 35,
-    fontWeight: 'bold',
-    color: 'blue',
-  },
-  innerBox: {
+  highlightButton: {
     height: 50,
-    width: 50,
-    backgroundColor: 'yellow',
-    margin: 10, // space around each of the elements
-    // you can also assign specific margins like marginBottom
-    justifyContent: 'center', // horizontally centered
-    alignItems: 'center', // vertically centered,
-    // padding: 10,
-    borderRadius: 25,
+    width: '30%',
+    backgroundColor: 'pink',
+    borderWidth: 1,
+    borderColor: 'red',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttontext: {
+    fontSize: 18,
   }
 });
-
-// notes during lecture
-
-// justify content is for major axis
-// alignItem is for minor axis
-
-// padding is inside the object
-// example is wearing a jacket -- the padding is inside the jacket,
-// so inside the object
